baae248da6589c446513413eea2d9c3c3fd20bde
ignazmba <115480022+ignazmba@users.noreply.github.com>
2022-11-02 17:18:18 -0400

    Add files via upload

---
+++ juve.groovy
@@ -0,0 +1,18 @@
+pipeline {
+    agent { "*"
+      }
+    triggers {
+        pollSCM '*'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                echo "Building.."
+                sh '''
+                cd git-tutorial-code/jenkins-101
+                touch mama.txt
+                '''
+           }
+        }
+    }
+}
